950ffff0b1974a41bac76da17881f5093418c672
Sierra Wallace <sskwallace@gmail.com>
2021-01-09 03:44:35 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,17 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+   name = "#{self.first_name} #{self.last_name}"
+   name
+  end
+
+  def list_roles
+    characters.collect do |ch|
+      "#{ch.name} - #{ch.show.name}"
+    end
+  end
+
+end
 end
